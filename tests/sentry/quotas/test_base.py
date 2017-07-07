f19c50ac7445de3af2f922f4d2037c5fd0bceaaa
David Cramer <dcramer@gmail.com>
2017-07-06 17:51:20 -0700

    quotas: refactor/expand tests

--- tests/sentry/quotas/test_base.py
+++ tests/sentry/quotas/test_base.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 from sentry.models import OrganizationOption, ProjectKey
-from sentry.quotas.base import Quota
+from sentry.quotas.base import BasicQuota, Quota
 from sentry.testutils import TestCase
 
 
@@ -37,3 +37,28 @@ class QuotaTest(TestCase):
         key = ProjectKey.objects.create(
             project=self.project, rate_limit_window=None, rate_limit_count=None)
         assert self.backend.get_key_quota(key) == (0, 0)
+
+    def test_account_limit(self):
+        org = self.create_organization()
+        project = self.create_project(organization=org)
+
+        OrganizationOption.objects.set_value(
+            org, 'sentry:account-rate-limit', 80,
+        )
+
+        with self.options({'system.rate-limit': 0}):
+            quotas = self.backend.get_quotas(project)
+            assert BasicQuota(
+                key='o:{}'.format(org.id),
+                limit=80,
+                window=3600,
+                reason_code='org_quota',
+            ) in quotas
+
+    def test_ignores_disabled_quotas(self):
+        org = self.create_organization()
+        project = self.create_project(organization=org)
+
+        with self.options({'system.rate-limit': 0}):
+            quotas = self.backend.get_actionable_quotas(project)
+            assert len(quotas) == 0
